d9fb0521266d78ac991ba573f58c2b25b9d04cf0
VFL72 <79771585+VFL72@users.noreply.github.com>
2021-06-23 12:09:07 -0400

    Delete script directory

--- script/booklet.js
+++
@@ -1,3 +0,0 @@
-if (screen.width <= 900) {
-    document.location = "../mobile-pages/booklet.html"
-}
--- script/schedule.js
+++
@@ -1,3 +0,0 @@
-if (screen.width <= 900) {
-    document.location = "../mobile-pages/schedule.html"
-}
